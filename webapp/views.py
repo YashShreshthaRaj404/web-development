5d0aec4498d4e8e624c8c87c39b272d5bb743fc9
Yash Shreshtha Raj <162735680+YashShreshthaRaj404@users.noreply.github.com>
2024-11-17 13:44:42 +0530

    added views.py

---
+++ webapp/views.py
@@ -0,0 +1,198 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("""
+                        <h1 style="font-size: 50px; text-align: center;">Yash Shreshtha Raj</h1> 
+                           <div style="font-size: 16px; text-align: center;">yashshreshtharaj14@gmail.com | +917618026063 | HIG-78 Saketpuri colony devkali Ayodhya</div> 
+                              <br style="font-size: 50px; text-align: center;">
+                                <div style="display: flex; align-items: center; margin-top: 10px;">
+                                    <a href="https://github.com/YashShreshthaRaj404" style="color: black; text-decoration: none; font-weight: bold; margin-right: 10px;">
+                                        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" style="width: 15px; height: 15px; vertical-align: middle; margin-right: 5px;">
+                                         github
+                                    </a style="font-size: 50px; text-align: center;">
+                                 <span style="margin: 0 10px;">|</span>
+                              <a href="https://www.linkedin.com/in/Yash-shreshtha-raj-a941252a6" style="color: black; text-decoration: none; font-weight: bold; margin-right: 10px;">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg" alt="LinkedIn" style="width: 15px; height: 15px; vertical-align: middle; margin-right: 5px;">
+                              linkedin
+                                 </a  style="text-align: center;">
+                                    <span style="margin: 0 10px;">|</span>
+                                        <a href="https://www.hackerrank.com/YashShreshthaRaj" style="color: black; text-decoration: none; font-weight: bold;">
+                                          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/HackerRank_logo.png" alt="HackerRank" style="width: 15px; height: 15px; vertical-align: middle; margin-right: 5px;">
+                                             hackerrank
+                                           </a style="font-size: 50px; text-align: center;">
+                                       </div>
+                                <br><br>                          
+                            <div style="font-size: 12px; text-align: left;" >EDUCATION</div>
+                            <p></p>
+                               <hr></hr>                        
+                                  <h4>PM Shree Kendriya Vidyalaya Ayodhya Cantt</h4> <div style="font-size: 15px; text-align: right;">Uttar Pradesh, India</div> 
+                                  <ch4>Computer Science, Artificial Intelligence, Physics, Chemistry & Math</ch4> <div style="font-size: 15px; text-align: right;">2023, 2026</div>
+                                  <div style="font-size: 12px; text-align: left;">SKILLS</div> 
+                             <hr></hr>
+                                 <div style="height: 4px;"></div> <!-- This line adds a gap of 20px -->
+                                   <div style="margin-top: 10px;">
+                                     <div style="text-align: left; margin-top: 20px;">
+                                       <span style="font-weight: bold; color: black;">Programming Languages:</span> Python </span><br>
+                                         <span style="font-weight: bold; color: black;">Libraries/Frameworks:</span> Django, Flask, NumPy </span><br>
+                                          <span style="font-weight: bold; color: black;">Tools / Platforms:</span> GitHub, VS Code, Git, NumPy, Web scraping </span><br>
+                                            <span style="font-weight: bold; color: black;">Databases:</span> SQL </span>
+                                         <div style="height: 9px;"></div> <!-- This line adds a gap of 20px -->
+                                    <div style="margin-top: 10px;">
+                                <div style="font-size: 12px; text-align: left;" >PROJECT / OPEN-SOURCE</div>                                     
+                           </div>
+                       <hr></hr>                         
+                           <div style="height: 9px;"></div> 
+                              <div style="margin-top: 10px;">
+                                 <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                   Binary-Code Converter | 
+                           <a href="https://github.com/YashShreshthaRaj404/Binary-code-Converter" style="color: black; text-decoration: none; margin-left: 6px;">Link</a>
+                       </h1>                      
+                           <div style="height: 5px;"></div> <!-- This line adds a gap of 10px -->
+                              <div style="margin-top: 8px;">
+                              <div style="margin-left: 30px;">
+                                                       
+                                <span style="color: black;"> Developed a robust </span> <span style="font-weight: bold; color: black;"> Binary Code Converter </span> that seamlessly translates binary code into
+                                    human-readable text (words, numbers, or sentences) and vice versa.<br> This tool allows for the conversion
+                                    of any alphanumeric data into its binary equivalent, enabling a deeper understanding of how computers
+                                    process information.<br> The project showcases proficiency in string manipulation, encoding/decoding
+                                    algorithms, and user interface design for erecient input/output operations.<br> The converter supports both
+                                    direct text-to-binary and binary-to-text transformations, providing a user-friendly experience for
+                                    learners, programmers, and tech enthusiasts.</p>
+                                    <p style="font-weight: bold;">Key Features:</p>
+                                       <span style="font-weight: bold; color: black;"> Bidirectional Conversion:</span> <span style="color: black;"> Converts binary to text and text (words, numbers, or sentences) to binary.</span><br>
+                                       <span style="font-weight: bold; color: black;"> Optimized Algorithms:</span> <span style="color: black;"> Ensures fast and accurate conversions.</span><br>
+                                       <span style="font-weight: bold; color: black;"> User-Centric Design:</span> <span style="color: black;"> Intuitive interface for easy input and output operations.</span><br>
+                                       <span style="font-weight: bold; color: black;"> Educational Tool:</span> <span style="color: black;"> Helps users understand the basics of binary encoding.</span><br>
+                                    <span style="font-weight: bold; color: black;">Technologies Used</span> <span style="color: black;"> [ List the technologies such as Python,Code Table, Logic Gates.]</span><br>
+                              </div>
+                        
+                            <div style="height: 9px;"></div> 
+                              <div style="margin-top: 13px;">
+                                 <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                   Bakery Management System | 
+                           <a href="https://github.com/YashShreshthaRaj404/Bakery-Managent-System" style="color: black; text-decoration: none; margin-left: 6px;">Link</a>
+                       </h1>                      
+                           <div style="height: 5px;"></div> <!-- This line adds a gap of 10px -->
+                              <div style="margin-top: 8px;">
+                              <div style="margin-left: 30px;">
+                                                
+                                <span style="color: black;"> The </span>  <span style="font-weight: bold; color: black;">Bakery Management System </span> <span style="color: black;">is a Python-based solution designed to streamline and automate the customer management process for a busy bakery store.<br>
+                                    This project addresses the challenges of handling large customer data manually by leveraging </span> <span style="font-weight: bold; color: black;">Python </span> along with the powerful libraries<span style="font-weight: bold; color: black;"> Pandas </span> and <span style="font-weight: bold; color: black;">OpenPyXL</span> <br> for efficient data processing and Excel automation.
+                                 
+                              <div style="height: 0px;"></div> <!-- This line adds a gap of 10px -->
+                                <div style="margin-top: 1px;">
+                                                                                         
+                                    <br>In this system, users can easily manage and track key details such as:<br>  
+                                       <div style="height: 5px;"></div>                        
+                                       <span style="font-weight: bold; color: black;">Customer Name</span><br>
+                                       <span style="font-weight: bold; color: black;">Item Name</span><br>
+                                       <span style="font-weight: bold; color: black;">Quantity of Items Purchased</span><br>
+                                       <span style="font-weight: bold; color: black;">Purchase Date</span><br>                       
+                           <div style="height: 12px;"></div>                                                                       
+                                <span style="color: black;"> The data is stored, processed, and automatically imported into separate Excel sheets for better organization and analysis.<br> 
+                                    This project eliminates the need for traditional manual record-keeping, reducing errors and saving time.</span><br>            
+                            <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                    <span style="font-weight: bold; color: black;">Key Features:</span><br>
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                     <span style="font-weight: bold; color: black;">Automated Data Entry:</span> <span style="color: black;"> Seamless handling of customer information using Pandas for fast data manipulation.</span><br>
+                           <div style="height: 9px;">
+                                     <span style="font-weight: bold; color: black;">Excel Automation:</span> <span style="color: black;"> Utilizes OpenPyXL for dynamic Excel file operations, including automatic data imports into separate sheets.</span><br>
+                           <div style="height: 7px;">
+                                     <span style="font-weight: bold; color: black;">Efficient Data Management:</span> <span style="color: black;"> Ensures quick and error-free recording of customer details, item names, quantities, and purchase dates.</span><br>
+                           <div style="height: 7px;">         
+                                     <span style="font-weight: bold; color: black;">User-Friendly Interface:</span> <span style="color: black;">Intuitive data entry and retrieval process for streamlined bakery management.</span><br>        
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">               
+                                    <span style="font-weight: bold; color: black;">Technologies Used:</span><br>
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                    <span style="font-weight: bold; color: black;">Programming Language:</span><span style="color: black;">Python</span><br>
+                                    <span style="font-weight: bold; color: black;">Libraries:</span><span style="color: black;">Pandas, OpenPyXL</span><br>
+                                    <span style="font-weight: bold; color: black;">File Format:</span><span style="color: black;"> (XLSX)</span><br>                         
+                        <div style="margin-left: -30px;">
+                        <div style="height: 9px;"></div> 
+                              <div style="margin-top: 13px;">
+                                 <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                    Kaun Banega Crorepati (KBC) | 
+                           <a href="https://github.com/YashShreshthaRaj404/Python-Programming-language-/blob/main/Kaun%20Banega%20Crorepati%20(KBC).py" style="color: black; text-decoration: none; margin-left: 6px;">Link</a>
+                       </h1>                      
+                           <div style="height: 5px;"></div> <!-- This line adds a gap of 10px -->
+                              <div style="margin-top: 8px;">
+                              <div style="margin-left: 30px;">
+                        
+                                <span style="color: black;">Developed a Python-based quiz game that simulates the popular TV show </span><span style="font-weight: bold; color: black;">Kaun Banega Crorepati
+                                      (KBC)</span>.<span style="color: black;">  The game allows users to answer multiple-choice questions in a structured format, advancing
+                                      through levels with increasing diffculty.</span><br><span style="color: black;">It incorporates a lifeline system, providing users with options
+                                      like "50-50" to help answer challenging questions. The game also features a reward system, where
+                                      players earn points or virtual money for each correct answer.</span><br>
+                              <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                    <span style="font-weight: bold; color: black;">Key Features:</span><br>
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                    <span style="color: black;"> Interactive question-and-answer gameplay.<br>
+                                                                 Lifelines such as 50-50 to assist players.<br>
+                                                                 Structured question progression with increasing diffculty.<br>
+                                                                 User-friendly console interface.</span><br>
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">               
+                                    <span style="font-weight: bold; color: black;">Technologies Used:</span><br>
+                           <div style="height: 18px;">
+                           <div style="margin-top: 8px;">
+                                    <span style="font-weight: bold; color: black;"> Python:</span><span style="color: black;">: Core programming language used for game logic, question management, and user interaction</span><br>   
+                                    <span style="font-weight: bold; color: black;"> File Handling:</span><span style="color: black;">Storing and retrieving questions from external files</span><br>
+                                    <span style="font-weight: bold; color: black;"> Control Flow (if-else statements):</span><span style="color: black;">Used to manage the quiz fow, checking answers, and implementing lifeline logic.</span><br>
+                                    <span style="font-weight: bold; color: black;"> Random Module:</span><span style="color: black;">Used for randomly selecting questions or lifeline outcomes.</span><br>                                
+                                  <div style="margin-left: -30px;">
+                                  <div style="height: 9px;"></div> 
+                              <div style="margin-top: 15px;">
+                     <div style="font-size: 12px; text-align: left;" >CERTIFICATIONS</div>                                      
+                 <hr></hr>                                                       
+                     <div style="height: 9px;"></div> <!-- This line adds a gap of 20px -->
+                         <div style="height: 5px;"></div> 
+                              <div style="margin-top: 11px;">
+                                 <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                      Python for Data Science - 
+                           <span style="font-weight: bold; color: black;"><a href="https://www.credly.com/badges/9853e30a-7099-4154-bfb2-c896879b79a0/public_url" style="color: black; text-decoration: none; margin-left: 6px;">IBM</a>
+                       </span><br>
+                             <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                      Google Analytics - 
+                           <span style="font-weight: bold; color: black;"><a href="https://analytics.google.com/analytics/academy/certificate/eaP9JRDzSe6oOyrhDSqp3w" style="color: black; text-decoration: none; margin-left: 6px;">Google</a>
+                       </span><br>
+                            <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                     Cloud Practitoner Essentials -
+                           <span style="font-weight: bold; color: black;"><a href="https://drive.google.com/file/d/1-H3PHykmadPhocp_rP_gcOBftxwIg8Yj/view?usp=drivesdk" style="color: black; text-decoration: none; margin-left: 6px;">Amazon Web Services</a>
+                       </span><br>
+                            <h1 style="font-size: 17px; text-align: left; display: inline; margin: 0;">
+                                    White Hat Hacker -
+                           <span style="font-weight: bold; color: black;"><a href="https://www.eduonix.com/certificate/2ed77c8db7" style="color: black; text-decoration: none; margin-left: 6px;">Eduonix</a>
+                       </span><br>
+                             <span style="color: black;">
+                                   Data Analysics with Python -</span>
+                           <span style="font-weight: bold; color: black;"><a href="https://freecodecamp.org/certification/fccf3d6317f-6cdf-453c-a373-0f0ea54c26b0/data-analysis-with-python-v7" style="color: black; text-decoration: none; margin-left: 6px;">Free Code Camp</a>
+                       </span><br>
+                       <div style="margin-left: 30px;">
+                                  <div style="height: 30px;"></div> 
+                              <div style="margin-top: 20px;">
+                       """)
+def resume_page(request):
+    return HttpResponse("<h1>Hey this is my Resume Page</h1>")
+
+
+
+                     
+
+
+
+
+
+
+
+
+
+
+
+
+
